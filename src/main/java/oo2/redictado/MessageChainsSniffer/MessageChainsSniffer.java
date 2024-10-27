5e5eba4bd6d6d14cbf98ad93cbd3916aa10730f3
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-10-27 20:34:52 -0300

    agregando el visitor junto con su clase para poder detectar el bad smell message chains

---
+++ src/main/java/oo2/redictado/MessageChainsSniffer/MessageChainsSniffer.java
@@ -0,0 +1,37 @@
+package oo2.redictado.MessageChainsSniffer;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import oo2.redictado.Aroma;
+import oo2.redictado.AromaReport;
+import oo2.redictado.CodeSniffer;
+import oo2.redictado.antlr4.BythonLexer;
+import oo2.redictado.antlr4.BythonParser;
+
+public class MessageChainsSniffer implements CodeSniffer {
+    public void sniff(String code, AromaReport report) {
+        // Creates Bython Parser
+        CharStream stream = CharStreams.fromString(code);
+        BythonLexer lexer = new BythonLexer(stream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        BythonParser parser = new BythonParser(tokens);
+
+        // Parses the code and checks for syntax errors
+        ParseTree tree = parser.program();
+        System.out.println(parser.getNumberOfSyntaxErrors());
+        if (parser.getNumberOfSyntaxErrors() > 0) {
+            throw new IllegalArgumentException("Syntax error");
+        }
+        
+        // Visits the parse tree to check for bad smells
+        MessageChainsSnifferVisitor visitor = new MessageChainsSnifferVisitor(report, "MessageChainsSniffer");
+        visitor.visit(tree);
+        
+        if (!report.stinks()) {
+            report.addAroma(new Aroma("MessageChainsSniffer", "El código huele bien", false));
+        }
+    }
+}
