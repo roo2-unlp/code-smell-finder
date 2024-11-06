5dce72545f1c67856563b3339f4d81ea3b033527
valentino <valentinoturconi@gmail.com>
2024-11-06 12:37:09 -0300

    terminado + correccion

--- src/test/java/oo2/redictado/LongMethodTest.java
+++ src/test/java/oo2/redictado/LongMethodTest.java
@@ -5,8 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/* IMPORTAR SNIFFER */
+
 public class LongMethodTest {
     
+    /* DECLARAR SNIFFER */
+
     public void testSuperaLineasConMetodos(){
         String code = """
         llamada.setOrigen(origen)
@@ -24,6 +28,7 @@ public class LongMethodTest {
         llamadas.add(get_llamada())
         """;
         AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
         assertFalse(report.stinks()); /*no daria ningun stink por eso es false*/
     }
 
@@ -43,6 +48,35 @@ public class LongMethodTest {
             resultado + oblea.precio();    
             """;
         AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
         assertTrue(report.stinks()); /*daria stink por eso es true*/
     }
+
+    public void testSuperaCaracteres(){
+
+        String code = 
+        """
+            if ((vehiculo1.getPeso() > vehiculo2.getPeso()) and (vehiculo1.getTamaño() > vehiculo2.getTamaño())
+                and (vehiculo1.getVelocidadPromedio < vehiculo2.getVelocidadPromedio())){
+            return true;
+            }
+            if ((vehiculo1.getCantPuertas() > vehiculo2.getCantPuertas()) and (vehiculo1.getEspacioBaul() > vehiculo2.getEspacioBaul())
+                and (vehiculo1.getCantidadLitrosNafta() > vehiculo2.getCantidadLitrosNafta())){
+            return true;
+            }
+            if ((vehiculo1.getCantidadCaballosFuerza() < vehiculo2.getCantidadCaballosFuerza()) and (vehiculo1.getVelocidadMaxima()
+                < vehiculo2.getVelocidadMaxima())) {
+                return true;    
+            }
+            
+            return false
+        """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertTrue(report.stinks()); /*daria stink por eso es true*/
+
+    }
+
+
+
 }
