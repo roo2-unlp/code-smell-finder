daa141d90be1cc3dd0d1e5aabc52c248e5cffe02
Sofia Martinez Coria <sofiamartinezcoria@gmail.com>
2024-11-13 17:10:57 -0300

    agrego mas casos de test

--- src/test/java/oo2/redictado/DuplicatedCodeSnifferTest.java
+++ src/test/java/oo2/redictado/DuplicatedCodeSnifferTest.java
@@ -87,6 +87,32 @@ public class DuplicatedCodeSnifferTest {
         assertFalse(report.stinks());
     }
 
+    public void testAtributosConValorDiferente(){
+        String code= """
+            class Coche() {
+            def __init__(self, marca, modelo, color, asientos) {
+                self.marca = marca;
+                self.modelo = modelo;
+                self.color = color;
+                self.encendido = False;
+                self.asientos = asientos;
+                self.velocidad = 0;
+                }
+            }
+
+            class Moto() {
+                def __init__(self, marca, modelo, color) {
+                    self.marca = "Yamaha";
+                    self.modelo = "Scooter";
+                    self.color = "Rojo";
+                }
+            }
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertFalse(report.stinks());
+    }
+    
     public void testMetodosRepetidos(){
         String code= """
             def suma(a, y) {
@@ -102,5 +128,165 @@ public class DuplicatedCodeSnifferTest {
         assertTrue(report.stinks());
         assertEquals(1, report.getAromas().size());
     }
+
+    public void testMetodosSinRepetir(){
+        String code= """
+            def suma(x, y) {
+                return x + y;
+            }
+
+            def resta(x, y) {
+                return x - y;
+            }
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertFalse(report.stinks());
+    }
+
+    public void testMetodoRepetidoEnOtro(){
+        String code= """
+            class Ejemplo() {
+
+                def metodoA() { 
+                    valor1 = 20.5;
+                    print("Inicio"); 
+                    resultado = 5 * 4;
+                    print("Fin"); 
+                }
+            
+                def metodoB() {
+                    print("Inicio"); 
+                    resultado = 5 * 4;
+                    print("Fin");
+                }
+            
+            }
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertTrue(report.stinks());
+        assertEquals(1, report.getAromas().size());
+    }
+
+    public void testMetodoRepetidoEnScript(){
+        String code= """
+            def inicio() {
+                print("1")
+                print("2")
+                print("3")
+            }
+
+            print("1")
+            print("2")
+            print("3")
+            suma = 1 + 3
+            print("suma")
+            resta = 4 - 2
+            print("resta")
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertTrue(report.stinks());
+        assertEquals(1, report.getAromas().size());
+    }
+
+    public void testMetodoSinRepetirEnScript(){
+        String code= """
+            def inicio() {
+                lista.add(x);
+                print(x); 
+            }
+
+            y= 5
+            x= 2
+            elem = x + y
+            print("suma" + elem)
+            resta = 4 - 2
+            print("resta")
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertFalse(report.stinks());
+    }
+
+    public void testScriptRepetido(){
+        String code= """
+           x = elem1 + elem2;
+           lista.add(x);
+           print(x);
+           print(lista);
+           x = elem1 + elem2;
+           lista.add(x);
+           print(x);
+           print(lista);
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertTrue(report.stinks());
+        assertEquals(1, report.getAromas().size());
+    }
+
+    public void testScriptSinRepetir(){
+        String code= """
+           x = elem1 + elem2;
+           lista.add(x);
+           print("algo");
+           print(x);
+           print(lista);
+           x = elem1 + elem2;
+           lista.add(x);
+           print(x);
+           print(lista);
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report); 
+        assertFalse(report.stinks());
+    }
+
+    public void testScriptSinRepetir(){
+        String code= """
+           SELECT columna1, columna2
+           WHERE columna3 = 'valor';
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report); 
+        assertFalse(report.stinks());
+    }
+
+    public void testScriptRepetido(){
+        String code= """
+            class Mujer(){
+                def __init__(self, nombre, apellido, edad) {
+                    self.nombre = nombre;
+                    self.apellido = apellido;
+                    self.edad = edad;
+                }
+                
+                def cumpleanios(self){
+                    self.edad += 1;
+                }
+            }
+
+            class Hombre() {
+                def __init__(self, nombre, apellido, edad) {
+                    self.nombre = nombre;
+                    self.apellido = apellido;
+                    self.edad = edad;
+                }
+                
+                def birthday(self){
+                    self.edad += 1;
+                }
+            }
+            """;
+        AromaReport report = new AromaReport(code);
+        codeSniffer.sniff(code, report);
+        assertTrue(report.stinks());
+        assertEquals(2, report.getAromas().size());
+    }
+
+
+
 
 }
