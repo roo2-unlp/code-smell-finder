bc28ab7ed6a807056d84432d175ea764f34680c7
Faustino Baudino Zoya <fausbaudino@gmail.com>
2024-11-29 17:38:55 -0300

    push

--- src/main/java/oo2/redictado/LongMethodCodeSniffer/LongMethodCodeVisitor.java
+++ src/main/java/oo2/redictado/LongMethodCodeSniffer/LongMethodCodeVisitor.java
@@ -6,12 +6,11 @@ import oo2.redictado.antlr4.BythonParser;
 import oo2.redictado.antlr4.BythonParserBaseVisitor;
 
 public class LongMethodCodeVisitor extends BythonParserBaseVisitor<Void> {
-    private static final int maxLineasPermitidas = 15;
-    private static final int maxCaracteresPermitidos = 500;
+    private static final int maxLineasPermitidas = 10;
+    private static final int maxCaracteresPermitidos = 100;
     private int lineasValidas = 0;
     private int caracteresValidos = 0;
-    private boolean esMetodoLlamado=false;
-    private boolean esMetodo = false;
+
 
     private AromaReport report;
     private String callerName;
@@ -25,44 +24,21 @@ public class LongMethodCodeVisitor extends BythonParserBaseVisitor<Void> {
     @Override
     public Void visitStatement(BythonParser.StatementContext ctx) {
 
-        if (esMetodo){
+        
             String texto = ctx.getText().trim();
-            if (esLineaValida(texto, ctx)) {
-                lineasValidas++;
-                caracteresValidos += texto.length();
-                if (lineasValidas > maxLineasPermitidas || caracteresValidos > maxCaracteresPermitidos) {
-                    report.addAroma(new Aroma(callerName,"el metodo excede la cantidad de lineas o caracteres por lo cual es un metodo largo",true));
-                    return null; //corto la ejecucion en el nodo actual una vez que se que hay mal olor
-                }
-            }
             
-        }   
+            lineasValidas++;
+            caracteresValidos += texto.length();
+            if (lineasValidas > maxLineasPermitidas || caracteresValidos > maxCaracteresPermitidos) {
+                report.addAroma(new Aroma(callerName,"el metodo excede la cantidad de lineas o caracteres por lo cual es un metodo largo",true));
+                return null; //corto la ejecucion en el nodo actual una vez que se que hay mal olor
+            }
+    
+            
+        
         return visitChildren(ctx); 
     }
 
-    public Void visitMethodDeclaration (BythonParser.MethodDeclContext ctx){
-        esMetodo = true;
-        visitChildren(ctx);
-        esMetodo = false;
-        return null;
-    }
 
 
-
-    private boolean esLlamadaAFuncion(BythonParser.StatementContext ctx) {
-        esMetodoLlamado=false;
-        if (ctx.getChildCount() > 0){
-            this.visit(ctx.getChild(0));
-        }
-        return esMetodoLlamado;
-    }
-
-    public Void visitMethodCall (BythonParser.MethodCallContext ctx){
-        esMetodoLlamado = true;
-        return null;
-    }
-
-    private boolean esLineaValida(String texto, BythonParser.StatementContext ctx) {
-        return !texto.startsWith("//") && !esLlamadaAFuncion(ctx) && !texto.startsWith("print");
-    }
 }
--- src/test/java/oo2/redictado/LongMethodTest.java
+++ src/test/java/oo2/redictado/LongMethodTest.java
@@ -14,98 +14,103 @@ public class LongMethodTest {
     
     /* DECLARAR SNIFFER */
     LongMethodCodeSniffer codeSniffer;
+
     @BeforeEach
     public void setUp(){
         codeSniffer = new LongMethodCodeSniffer();
     }
+
+    @Test
     public void testSuperaLineasConMetodos(){
         String code = """
-        llamada.setOrigen(origen)
-        llamada.setDestino(destino)
-        llamada.setDuracion(duracion)
-
-        if duracion > 100:
-            llamada.setExtra(50)
-        else:
-            llamada.setExtra(0)
-
-        llamada.impuestos(origen, destino)  // Corrobora que ambos números sean del mismo país y ejecuta métodos internos
-        llamada.calcular_costo(tipo)        // Calcula el costo dependiendo de si es fija o móvil
-        llamada.calcular_credito()         // Calcula la deuda restante (con variables ya modificadas)
-        llamadas.add(get_llamada())
+        llamada.setOrigen(origen);
+        llamada.setDestino(destino);
+        llamada.setDuracion(duracion);
+        if (duracion > 100) {
+            llamada.setExtra(50);
+        } else {
+            llamada.setExtra(0);
+        }
+        llamada.impuestos(origen, destino);
+        llamada.calcular_costo(tipo);
+        llamada.calcular_credito();
+        llamadas.add(get_llamada());
         """;
+
         AromaReport report = new AromaReport(code);
         codeSniffer.sniff(code, report);
-        assertFalse(report.stinks()); /*no daria ningun stink por eso es false*/
+        assertTrue(report.stinks()); /*no daria ningun stink por eso es false*/
     }
 
+    @Test
     public void testSuperaLineasSinMetodos(){
         String code = 
             """
-            resultado + alfajor precio(); 
-            resultado + Cocacola.precio();
-            resultado + galletitas.precio(); 
-            resultado + barradecereal.precio();
-            resultado + medialuna.precio(); 
-            resultado + cereales.precio();
-            resultado + gomitas.precio();
-            resultado + saladix.precio();
-            resultado + chicle.precio();
-            resultado + mentos.precio(); 
-            resultado + oblea.precio();    
+            resultado + alfajor.precio; 
+            resultado + cocacola.precio;
+            resultado + galletitas.precio; 
+            resultado + barradecereal.precio;
+            resultado + medialuna.precio; 
+            resultado + cereales.precio;
+            resultado + gomitas.precio;
+            resultado + saladix.precio;
+            resultado + chicle.precio;
+            resultado + mentos.precio; 
+            resultado + oblea.precio;
             """;
         AromaReport report = new AromaReport(code);
         codeSniffer.sniff(code, report);
         assertTrue(report.stinks()); /*daria stink por eso es true*/
     }
 
+    @Test
     public void testSuperaCaracteres(){
 
         String code = 
         """
-            if ((vehiculo1.getPeso() > vehiculo2.getPeso()) and (vehiculo1.getTamaño() > vehiculo2.getTamaño())
-                and (vehiculo1.getVelocidadPromedio < vehiculo2.getVelocidadPromedio())){
-            return true;
+            if ((vehiculo1.getPeso() > vehiculo2.getPeso() and vehiculo1.getTamanio() > vehiculo2.getTamanio)
+                and (vehiculo1.getVelocidadPromedio() < vehiculo2.getVelocidadPromedio())){
+                return true;
             }
             if ((vehiculo1.getCantPuertas() > vehiculo2.getCantPuertas()) and (vehiculo1.getEspacioBaul() > vehiculo2.getEspacioBaul())
                 and (vehiculo1.getCantidadLitrosNafta() > vehiculo2.getCantidadLitrosNafta())){
-            return true;
+                return true;
             }
             if ((vehiculo1.getCantidadCaballosFuerza() < vehiculo2.getCantidadCaballosFuerza()) and (vehiculo1.getVelocidadMaxima()
                 < vehiculo2.getVelocidadMaxima())) {
                 return true;    
             }
             
-            return false
+            return false;
         """;
         AromaReport report = new AromaReport(code);
         codeSniffer.sniff(code, report);
         assertTrue(report.stinks()); /*daria stink por eso es true*/
     }
 
+    @Test
     public void testAsignaciones(){
 
-        String code = 
-        """
-        
+        String code = """
         monitor1 = 20000;
         monitor2 = 10000;
         mouse = 5000;
         teclado = 7000;
         procesador = 20000;
         placaVideo = 30000;
-        ram = 15000
+        ram = 15000;
         fuente = 10000;
         gabinete = 17000;
         coolers = 3000;
         resultado = monitor1 + monitor2 + mouse + teclado + procesador + placaVideo + ram + fuente + gabinete + coolers;
-
         """;
+
         AromaReport report = new AromaReport(code);
         codeSniffer.sniff(code, report);
         assertTrue(report.stinks()); /*daria stink por eso es true*/
     }
 
+    @Test
     public void testCorrecto(){
 
         String code =
